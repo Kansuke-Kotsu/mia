d1cdcaeeb5b08b73fd802b07a7da1579895163b4
Kansuke-Kotsu <85889110+Kansuke-Kotsu@users.noreply.github.com>
2023-01-04 23:33:33 +0900

    20230105
    can mia(base)

--- mia.py
+++ mia.py
@@ -2,43 +2,46 @@
 import os
 import numpy as np
 import tensorflow as tf
-from keras.models import load_model
-import keras.backend as K
+from tensorflow import keras
+from tensorflow.python.keras.models import load_model
+import matplotlib.pyplot as plt
+import time
 
 '''config'''
 id = 0
-rate = - 0.01
-epoch = 50
-model = load_model('model/')
-
-def cal_grad(model, x, y):
-    class_output = model.output[:,5]  # Tensor / クラススコア
-    grad_tensor = K.gradients(class_output, model.input)[0]  # Tensor / クラススコアに対する入力の勾配
-    grad_func = K.function([model.input], [grad_tensor])  #  勾配の値を算出するための関数
-    x= np.expand_dims(train_images[0], axis=0)  #画像自体は大きさが(28,28) なので(1,28,28)にする
-    grad = grad_func([x])[0][0]  # ndarray: 算出された勾配の値
-    loss = tf.keras.losses.binary_crossentropy(x, y)
-    return class_output, loss, grad
+rate = 1
+epoch = 100
+model_params = "model/model_0050.h5"
+cal_grad = tf.keras.losses.CategoricalCrossentropy()
 
 def main():
+    model = load_model(model_params)
     # vector ==> image
-    z = tf.random.normal([28, 28], 0, 1, tf.float32, seed=1)
-    #print(z)
-    #print(z.shape)
+    z = tf.random.normal((1, 28, 28), 0, 1, tf.float32, seed=1)
 
     # mia
     for i in range(epoch):
-        y = id * np.ones()
-        x = model.predict(z)
-        # 勾配計算
-        class_output, loss, grad = cal_grad(model=model, x=x, y=y)
-        #print(grad)
-        # 逆伝播
+        y = np.zeros(9, dtype=np.float32)
+        y = np.insert(y, id, 1)
+        y = tf.multiply(y, 1)
+
+        plt.imshow(z[0])
+        plt.savefig('result/image_{:04d}.png'.format(i))
+        # 元の入力テンソル x に対する z の微分
+        with tf.GradientTape() as t:
+            t.watch(z)
+            x = model(z)[0]
+            print(x[0])
+            loss = cal_grad(y, x)
+        grad = t.gradient(loss, z)
         z -= rate * grad
-        print('{0}回目: ', format(i+1))
-        print('score: {0}', format(class_output))
 
 
-main()
+
+
+
+
+if __name__ == '__main__':
+    main()
 
     
